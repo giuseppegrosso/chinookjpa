c7337bc56642a16125e9cbfc8eff6b28ed913524
Giuseppe Grosso <g.grosso@quidinfo.it>
2021-03-07 15:03:29 +0100

    conclusione dei test

--- src/test/java/it/plansoft/chinookjpa/AlbumsTest.java
+++ src/test/java/it/plansoft/chinookjpa/AlbumsTest.java
@@ -3,26 +3,69 @@ package it.plansoft.chinookjpa;
 import it.plansoft.chinookjpa.interfaces.BaseCrudRepositoryTest;
 import it.plansoft.chinookjpa.model.Albums;
 import it.plansoft.chinookjpa.repository.AlbumRepository;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
+import it.plansoft.chinookjpa.service.AlbumService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class AlbumsTest extends BaseCrudRepositoryTest<
+        AlbumService,
         AlbumRepository,
         Albums, Long> {
 
+    @Autowired
+    private AlbumService service;
+    @Autowired
+    private AlbumRepository repository;
 
-    public AlbumsTest(
-            AlbumRepository repo,
-            Albums model) {
-        super(repo, model);
+    @Test
+    public void testRepository() {
+        setRepository(service, repository, new Albums());
+        insertRepo();
+        findAll();
+        Optional<Albums> a = getRepoById(2L);
+        deleteRepo(a.get());
+
+        deleteRepoById(1L);
+        findAll();
+
+    }
+
+
+    @Test
+    public void testService() {
+        setRepository(service, repository, new Albums());
+        getServiceinsert();
+        getServiceAll();
+        getServiceById(2L);
+        deleteService(new Albums(2L, "", null));
+
+        deleteServiceById(1L);
+        getServiceAll();
     }
 
 
     @Override
-    protected Albums getInsertElement() {
-        return new Albums(1L, "prova di album",
+    protected List<Albums> getInsertElement() {
+        return Arrays.asList(
+                new Albums(1L, "prova di album",
                 null
-        );
+        ),
+                new Albums(2L, "prova di album 2",
+                        null
+                ),
+                new Albums(3L, "prova di album 3",
+                        null
+                ));
     }
 
 
--- src/test/java/it/plansoft/chinookjpa/interfaces/BaseCrudRepositoryTest.java
+++ src/test/java/it/plansoft/chinookjpa/interfaces/BaseCrudRepositoryTest.java
@@ -1,22 +1,27 @@
 package it.plansoft.chinookjpa.interfaces;/* ggrosso created on 06/03/2021 inside the package - it.plansoft.chinookjpa.interfaces */
 
 import it.plansoft.chinookjpa.model.BaseId;
+import it.plansoft.chinookjpa.service.BaseCrudService;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public abstract class BaseCrudRepositoryTest<REPOSITORY extends JpaRepository<MODEL, ID>, MODEL extends BaseId<ID>, ID> {
+public abstract class BaseCrudRepositoryTest<
+        SERVICE extends BaseCrudService<REPOSITORY, MODEL, ID>,
+        REPOSITORY extends JpaRepository<MODEL, ID>,
+        MODEL extends BaseId<ID>, ID> {
 
+    protected SERVICE service;
     protected REPOSITORY repository;
     protected MODEL model;
 
-    protected abstract MODEL getInsertElement();
+    protected abstract List<MODEL> getInsertElement();
 
-    public BaseCrudRepositoryTest(REPOSITORY repository, MODEL model) {
+    public void setRepository(SERVICE service, REPOSITORY repository, MODEL model) {
+        this.service = service;
         this.repository = repository;
         this.model = model;
     }
@@ -36,4 +41,97 @@ public abstract class BaseCrudRepositoryTest<REPOSITORY extends JpaRepository<MO
         // check di lettura id
         assertEquals(e.get().getId(), r.getId());
     }
+
+
+    public void insertRepo() {
+        this.repository.saveAll(getInsertElement());
+    }
+
+
+    public Optional<MODEL> getRepoById(ID id) {
+        Optional<MODEL> rlist = repository.findById(id);
+        assertNotNull(rlist);
+
+        return rlist;
+    }
+
+    public void deleteRepo(MODEL model) {
+        List<MODEL> rlist = repository.findAll();
+        assertTrue(rlist.size() > 0);
+
+        repository.delete(model);
+
+        List<MODEL> rlist2 = repository.findAll();
+        assertTrue(rlist.size() > 0);
+
+        assertEquals(rlist.size() - 1, rlist2.size());
+
+    }
+
+    public void deleteRepoById(ID id) {
+        List<MODEL> rlist = repository.findAll();
+        assertTrue(rlist.size() > 0);
+
+        repository.deleteById(id);
+
+        List<MODEL> rlist2 = repository.findAll();
+        assertTrue(rlist.size() > 0);
+
+        assertEquals(rlist.size() - 1, rlist2.size());
+
+    }
+
+
+    /********************************************************************************
+     * TEST SERVICE
+     *********************************************************************************/
+
+    public void getServiceById(ID id) {
+        Optional<MODEL> r = service.findById(id);
+        assertNotNull(r);
+        assertNotNull(r.get());
+
+        // check di lettura id
+        assertEquals(r.get().getId(), id);
+    }
+
+    public void getServiceAll() {
+
+        List<MODEL> rlist = service.findAll();
+
+        assertTrue(rlist.size() > 0);
+    }
+
+    public void getServiceinsert() {
+
+        List<MODEL> mm = getInsertElement();
+
+        List<MODEL> mi = service.saveAll(mm);
+        for (MODEL m : mi) {
+            Optional<MODEL> e = service.findById(m.getId());
+
+            assertNotNull(e);
+            assertNotNull(e.get());
+            // check di lettura id
+            assertEquals(e.get().getId(), m.getId());
+        }
+    }
+
+    public void deleteService(MODEL model) {
+        deleteServiceById(model.getId());
+    }
+
+    public void deleteServiceById(ID id) {
+        List<MODEL> rlist = service.findAll();
+
+        assertTrue(rlist.size() > 0);
+
+        service.delete(model);
+
+        List<MODEL> rlist2 = service.findAll();
+
+        assertTrue(rlist2.size() > 0);
+        assertEquals(rlist.size() - 1, rlist2.size());
+    }
+
 }
